ab38110ef7cf2e8f7ec29e8190d9b37b30291822
Fabrício Sampaio <fabricio-sampaio@usp.br>
2022-07-16 16:35:01 -0300

    Update exercicio2b.c

--- exercicio2b.c
+++ exercicio2b.c
@@ -7,6 +7,7 @@ Pedro Lucas Castro de Andrade - 11212289
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <assert.h>
 #include <string.h>
 #include <math.h>
 
@@ -29,6 +30,9 @@ typedef char elem;
 typedef struct {
     elem dados[20];
 }hash;
+
+// Numero de repeticoes necessarias
+#define num_rep 3
 
 unsigned converter(string s) {
    unsigned h = 0;
@@ -72,6 +76,24 @@ unsigned h_mul(unsigned x, unsigned i, unsigned B) {
     return  ((int) ((fmod(x * A, 1) * B) + i)) % B;
 }
 
+double desvio_padrao(double* tempos)
+{
+    double sum = 0;
+    for (int i = 0; i < num_rep; i++) {
+        sum += tempos[i];
+    }
+    double media = sum / num_rep;
+
+    double dp = 0;
+    for (int i = 0; i < num_rep; i++) {
+        dp += (pow((tempos[i] - media),2));
+    }
+    dp = dp / num_rep;
+    dp = sqrt(dp);
+
+    return dp;
+}
+
 hash* criar_tabela(int B) {
     hash* tabela = malloc(sizeof(hash) * B);
     if (tabela == NULL) { //problema na alocacao de memoria
@@ -79,7 +101,7 @@ hash* criar_tabela(int B) {
         exit(-1);
     }
     for (int i = 0; i < B; i++) {
-        strcpy(tabela[i].dados, "VAZIO\0"); // indica que o bucket estah disponivel
+        strcpy(tabela[i].dados, "VAZIO\0"); // indica que o bucket esta disponivel
     }
     return tabela;
 }
@@ -94,11 +116,12 @@ int inserir_tabela(hash* tabela_hash, string inserir, int B) {
     for (int i = 0; i < B;  i++) {
         pos = (h_mul(string_num, i, B) + i * h_div(string_num, i, B)) % B;
 
-        if (!(strcmp(tabela_hash[pos].dados, "VAZIO"))) {
+        if (!(strcmp(tabela_hash[pos].dados, "VAZIO"))) { 
             strcpy(tabela_hash[pos].dados, inserir);
-            return (i == 0) ? 0 : 1; // se precisou procurar mais de uma vez, houve colisao
+             return (i == 0) ? 0 : 1; // se precisou procurar mais de uma vez, houve colisao
         }
         if (!(strcmp(tabela_hash[pos].dados, inserir))) {
+            //colisoes = 1; // ISSO SERIA CONSIDERADO UMA COLISÃO ???
             return 0; // elemento repetido
         }
     }
@@ -131,32 +154,50 @@ int main(int argc, char const *argv[]) {
     string* insercoes = ler_strings("strings_entrada.txt", N);
     string* consultas = ler_strings("strings_busca.txt", M);
 
-    // cria tabela hash com hash por hash duplo
-    hash* tabela_hash = criar_tabela(B);
 
-    // inserção dos dados na tabela hash
-    inicia_tempo();
-    for (int i = 0; i < N; i++) {
-        // inserir insercoes[i] na tabela hash
-        colisoes += inserir_tabela(tabela_hash, insercoes[i], B);
+    double soma_clocks_insercao = 0;
+    double soma_clocks_busca = 0;
+    double tempos_insercao[num_rep];
+    double tempos_busca[num_rep];
+
+    for(int k = 0; k < num_rep; k++) {
+
+        // cria tabela hash com hash por hash duplo
+        hash* tabela_hash = criar_tabela(B);
+
+        // inserção dos dados na tabela hash
+        inicia_tempo();
+        for (int i = 0; i < N; i++) {
+            // inserir insercoes[i] na tabela hash
+            colisoes += inserir_tabela(tabela_hash, insercoes[i], B);
+        }
+        tempos_insercao[k] = finaliza_tempo();
+        soma_clocks_insercao += tempos_insercao[k];
+        printf("Insercao numero : %d -> Tempo de busca:  %fs\n", k+1, tempos_insercao[k]);
+
+
+        // busca dos dados na tabela hash
+        inicia_tempo();
+        for (int i = 0; i < M; i++) {
+            // buscar consultas[i] na tabela hash
+            encontrados += busca_tabela(tabela_hash, consultas[i], B);
+        }
+        tempos_busca[k] = finaliza_tempo();
+        soma_clocks_busca += tempos_busca[k];
+        printf("Busca numero    : %d -> Tempo de busca:  %fs\n", k+1, tempos_busca[k]);
+
+        // limpa a tabela hash
+        liberar_tabela(tabela_hash, B);
+
     }
-    double tempo_insercao = finaliza_tempo();
 
-    // busca dos dados na tabela hash
-    inicia_tempo();
-    for (int i = 0; i < M; i++) {
-        // buscar consultas[i] na tabela hash
-        encontrados += busca_tabela(tabela_hash, consultas[i], B);
-    }
-    double tempo_busca = finaliza_tempo();
 
-    // limpa a tabela hash
-    liberar_tabela(tabela_hash, B);
-
-    printf("Colisoes na insercao: %d\n", colisoes);
-    printf("Tempo de insercao   : %fs\n", tempo_insercao);
-    printf("Tempo de busca      : %fs\n", tempo_busca);
-    printf("Itens encontrados   : %d\n", encontrados);
+    float desvio_insercao = desvio_padrao(tempos_insercao);
+    float desvio_busca = desvio_padrao(tempos_busca);
+    printf("\n--> Tempo medio de insercao   :\t%fs\n", soma_clocks_insercao/num_rep);
+    printf("--> Desvio padrao da insercao :\t%f\n", desvio_insercao);
+    printf("--> Tempo medio de busca      :\t%fs\n", soma_clocks_busca/num_rep);
+    printf("--> Desvio padrao da busca    :\t%f\n", desvio_busca);
 
     return 0;
 }
